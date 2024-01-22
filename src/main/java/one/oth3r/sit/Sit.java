189ae2d2b587ebb06f286e634d77dac82960666f
Oth3r <oth3rone@outlook.com>
2024-01-22 17:56:34 -0600

    new packet system 1.20.5

--- src/main/java/one/oth3r/sit/Sit.java
+++ src/main/java/one/oth3r/sit/Sit.java
@@ -3,23 +3,24 @@ package one.oth3r.sit;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import io.netty.buffer.ByteBuf;
-import io.netty.util.ReferenceCountUtil;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Sit implements ModInitializer {
@@ -39,16 +40,22 @@ public class Sit implements ModInitializer {
 		config.load();
 		Events.register();
 		//PACKETS
-		ServerPlayNetworking.registerGlobalReceiver(PacketBuilder.getIdentifier(),
-				(server, player, handler, buf, responseSender) -> {
-			// copy to not throw errors
-			PacketBuilder packet = new PacketBuilder(buf.copy());
+		PayloadTypeRegistry.playC2S().register(SettingsPayload.ID, SettingsPayload.CODEC);
+		ServerPlayNetworking.registerGlobalReceiver(SettingsPayload.ID,((payload, context) -> {
 			server.execute(() -> {
-						Type hashMapToken = new TypeToken<HashMap<String, Object>>() {}.getType();
-						Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-						playerSettings.put(player,gson.fromJson(packet.getMessage(),hashMapToken));
-					});
-				});
+				Type hashMapToken = new TypeToken<HashMap<String, Object>>() {}.getType();
+				Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+				playerSettings.put(context.player(),gson.fromJson(payload.value,hashMapToken));
+			});
+		}));
+	}
+	public record SettingsPayload(String value) implements CustomPayload {
+		public static final CustomPayload.Id<SettingsPayload> ID = new CustomPayload.Id<>(new Identifier(MOD_ID,"settings_v1.1"));
+		public static final PacketCodec<RegistryByteBuf, SettingsPayload> CODEC = PacketCodecs.STRING.xmap(SettingsPayload::new, SettingsPayload::value).cast();
+		@Override
+		public Id<SettingsPayload> getId() {
+			return ID;
+		}
 	}
 	public static MutableText lang(String key, Object... args) {
 		if (isClient) return Text.translatable(key, args);
