4ec37db7b0dc5f71fc93e97c5a7107f27c3fd8b1
Oth3r <oth3rone@outlook.com>
2024-04-29 12:38:33 -0500

    Revert "new packet system 1.20.5"
    This reverts commit fdd7aeaf4fb56464efab7987bf8d04680a5d42a3.

--- src/main/java/one/oth3r/sit/SitClient.java
+++ src/main/java/one/oth3r/sit/SitClient.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.network.packet.CustomPayload;
 
 public class SitClient implements ClientModInitializer {
     public static boolean inGame = false;
@@ -15,13 +13,13 @@ public class SitClient implements ClientModInitializer {
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             inGame = true;
             // send a data packet whenever joining a server
-            ClientPlayNetworking.send(sendPackets());
+            client.execute(SitClient::sendPackets);
         });
         // reset inGame
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> inGame = false);
     }
-    public static CustomPayload sendPackets() {
+    public static void sendPackets() {
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        return new Sit.SettingsPayload(gson.toJson(Utl.getHandSettings()));
+        new PacketBuilder(gson.toJson(Utl.HandSettings.getHandSettings())).send();
     }
 }
