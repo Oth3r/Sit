1d08ee6775bb848c76eaa6164eb5b30656e2fe53
Oth3r <oth3rone@outlook.com>
2024-05-16 15:09:25 -0500

    fixed changing settings mid-game not sending updates to the server

--- src/main/java/one/oth3r/sit/SitClient.java
+++ src/main/java/one/oth3r/sit/SitClient.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.network.packet.CustomPayload;
 import one.oth3r.sit.packet.CustomPayloads;
 
 public class SitClient implements ClientModInitializer {
@@ -16,14 +15,19 @@ public class SitClient implements ClientModInitializer {
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             inGame = true;
             // send a data packet whenever joining a server
-            ClientPlayNetworking.send(sendPackets());
+            sendSettingsPackets();
         });
         // reset inGame
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> inGame = false);
     }
 
-    public static CustomPayload sendPackets() {
-        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        return new CustomPayloads.SettingsPayload(gson.toJson(Utl.HandSettings.getHandSettings()));
+    /**
+     * sends the settings packets to the server is the client is in game
+     */
+    public static void sendSettingsPackets() {
+        if (inGame) {
+            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+            ClientPlayNetworking.send(new CustomPayloads.SettingsPayload(gson.toJson(Utl.HandSettings.getHandSettings())));
+        }
     }
 }
