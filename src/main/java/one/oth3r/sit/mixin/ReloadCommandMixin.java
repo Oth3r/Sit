58fd5e7cf84eb1a33f20fd4cbe8b9227cb11a849
Oth3r <oth3rone@outlook.com>
2024-09-30 14:28:12 -0500

    reload message to all player with perms when `/reload` is ran

--- src/main/java/one/oth3r/sit/mixin/ReloadCommandMixin.java
+++ src/main/java/one/oth3r/sit/mixin/ReloadCommandMixin.java
@@ -1,9 +1,14 @@
 package one.oth3r.sit.mixin;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ReloadCommand;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Formatting;
 import one.oth3r.sit.file.FileData;
+import one.oth3r.sit.utl.Data;
+import one.oth3r.sit.utl.Utl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,5 +19,16 @@ public class ReloadCommandMixin {
     @Inject(at = @At("TAIL"), method = "register")
     private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CallbackInfo ci) {
         FileData.loadFiles();
+
+        // make sure the server isn't null
+        MinecraftServer server = Data.getServer();
+        if (server == null || server.getPlayerManager() == null) return;
+
+        // send a reloaded message to all players with permissions
+        for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+             if (player.isCreativeLevelTwoOp()) {
+                 player.sendMessage(Utl.messageTag().append(Utl.lang("msg.reloaded").formatted(Formatting.GREEN)));
+             }
+        }
     }
 }
